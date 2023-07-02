88bdd90a4e0f5945ecd2e405ff9857be2eac5a48
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-07-02 19:02:39 +0200

    chore: added layout for Start page.

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,14 +1,18 @@
 import { motion } from 'framer-motion';
 
+import styles from './Start.module.css';
+
 const Start = (): JSX.Element => {
   return (
     <motion.div
+      className={styles.root}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 3 }}
     >
-      Start
+      <section className={styles.left}>left</section>
+      <section className={styles.right}>right</section>
     </motion.div>
   );
 };
